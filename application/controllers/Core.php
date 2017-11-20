326b5fee2e795935f1547c3226f7e7ae6ebd54be
unknown <chandra.comment@gmail.com>
2017-11-20 12:02:39 +0700

    fix campaign

--- application/controllers/Core.php
+++ application/controllers/Core.php
@@ -423,7 +423,7 @@ class Core extends Main_Controller {
         $param=array('url'=>$this->url_airpush,
                     'key'=>$this->key_airpush,
                     'method'=>"getCampaignsByUserId");
-        $this->data['list']=$this->platform->airpush($param);
+        $this->data['list']=(array) json_decode($this->platform->airpush($param));
         print_r($this->data['list']);die;
         $this->data['campcat']=$this->basedata->getCampCat();
         $this->tempe->load($this->theme.'/modul',$this->theme.'/platform/camp_airpush',$this->data);
